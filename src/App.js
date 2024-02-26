0e65bf60adf497f7c47bfb5476092e3db363649a
Ashish <rbzashu1@gmail.com>
2024-02-26 12:37:51 +0530

    created sign in and sign up form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
+import React from 'react';
+import Body from './components/Body'
+
+
 
 function App() {
+  
+
   return(
-    <div className="bg-red-900 h-48 w-48">hiiiiii</div>
+    <Body/>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import App from './App';
 import './index.css'
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App  />
   </React.StrictMode>
 );
 
